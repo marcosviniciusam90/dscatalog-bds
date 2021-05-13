d46e37e1ba198987a2d0133ff6f26258661ee267
Marcos <marcos.mendonca@matera.com>
2021-05-12 23:55:30 -0300

    Auditing data

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -1,12 +1,10 @@
 package com.devsuperior.dscatalog.entities;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.time.Instant;
 
 @Data
 @Builder
@@ -21,4 +19,22 @@ public class Category implements Serializable {
     private Long id;
 
     private String name;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE") //padrão UTC
+    @Setter(AccessLevel.NONE) //para não gerar método setter
+    private Instant createdAt;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE") //padrão UTC
+    @Setter(AccessLevel.NONE) //para não gerar método setter
+    private Instant updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        createdAt = Instant.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        updatedAt = Instant.now();
+    }
 }
