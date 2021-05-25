8a3fe18171858b939a35fe107cb30aa25d39e2e0
Marcos <marcos.mendonca@matera.com>
2021-05-25 06:19:42 -0300

    Testes automáticos do ProductRepository

---
+++ backend/src/test/java/com/devsuperior/dscatalog/utils/Factory.java
@@ -0,0 +1,51 @@
+package com.devsuperior.dscatalog.utils;
+
+import com.devsuperior.dscatalog.dto.CategoryDTO;
+import com.devsuperior.dscatalog.dto.ProductDTO;
+import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.entities.Product;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+
+public class Factory {
+    public static Product createProduct() {
+        return Product.builder()
+                .id(1L)
+                .name("Phone")
+                .description("Good Phone")
+                .price(800.0)
+                .imgUrl("https://img.com/img.png")
+                .date(Instant.parse("2021-05-25T03:00:00Z"))
+                .categories(new HashSet<>(Collections.singletonList(createCategory())))
+                .build();
+    }
+
+    public static ProductDTO createProductDTO() {
+        return ProductDTO.builder()
+                .id(1L)
+                .name("Phone")
+                .description("Good Phone")
+                .price(800.0)
+                .imgUrl("https://img.com/img.png")
+                .date(Instant.parse("2021-05-25T03:00:00Z"))
+                .categories(new ArrayList<>(Collections.singletonList(createCategoryDTO())))
+                .build();
+    }
+
+    public static Category createCategory() {
+        return Category.builder()
+                .id(2L)
+                .name("Electronics")
+                .build();
+    }
+
+    public static CategoryDTO createCategoryDTO() {
+        return CategoryDTO.builder()
+                .id(2L)
+                .name("Electronics")
+                .build();
+    }
+}
